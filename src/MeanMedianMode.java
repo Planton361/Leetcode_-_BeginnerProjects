b1fec9b0eadb7ff85db35de926f90fd5a0156faf
Anton Platonov <92857602+Planton361@users.noreply.github.com>
2024-11-12 21:16:20 +0100

    Updating Project

--- src/MeanMedianMode.java
+++ src/MeanMedianMode.java
@@ -8,6 +8,148 @@ If there is an even number of numbers in the list, return both numbers that coul
 If there are multiple modes, return all of them.*/
 
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class MeanMedianMode {
+    public static void main(String[] args) {
+        MeanMedianModeController meanMedianModeController = new MeanMedianModeController();
+        meanMedianModeController.runMeanMedianMode();
+    }
+}
+
+class MeanMedianModeController {
+    private final MeanMedianModeModel meanMedianModeModel;
+    private final MeanMedianModeView meanMedianModeView;
+    private static boolean continueLoop = true;
+
+    public MeanMedianModeController() {
+        meanMedianModeModel = new MeanMedianModeModel();
+        meanMedianModeView = new MeanMedianModeView();
+
+    }
+
+    public void runMeanMedianMode() {
+        meanMedianModeView.announceMessage(MeanMedianModeView.WELCOME_MESSAGE);
+        while (continueLoop) {
+            String userInput = meanMedianModeView.askUserForNumber();
+            double[] numbers = meanMedianModeModel.createArrayOfUserInput(userInput);
+
+            meanMedianModeView.announceMessage(MeanMedianModeView.OPTIONS_PROGRAM);
+            int option = meanMedianModeView.getOptionInput();
+            switch (option) {
+                case 1:
+                    double mean = meanMedianModeModel.meanOfList(numbers);
+                    meanMedianModeView.announceMessage(String.valueOf(mean));
+                    break;
+
+                case 2:
+                    double mode = meanMedianModeModel.modeOfList(numbers);
+                    meanMedianModeView.announceMessage(String.valueOf(mode));
+                    break;
+                case 3:
+
+                    break;
+            }
+
+
+            continueLoop = askUserToContinueProgram();
+        }
+
+        meanMedianModeView.announceMessage(MeanMedianModeView.GOODBYE_MESSAGE);
+    }
+
+    public boolean askUserToContinueProgram() {
+        meanMedianModeView.announceMessage(MeanMedianModeView.CONTINUE_PROGRAM);
+        int userInput = meanMedianModeView.askUserToContinue();
+        return userInput == 1;
+    }
+
+
+}
+
+class MeanMedianModeView {
+    Scanner scanner = new Scanner(System.in);
+    public static final String WELCOME_MESSAGE = "Welcome! In this program you can get the MeanMedianMode of your given numbers.";
+    public static final String CONTINUE_PROGRAM = "Do you want to continue?\nPress 1 for yes.\nPress 0 for no.";
+    public static final String OPTIONS_PROGRAM = "What do you want to do with the numbers?\n1 for average\n2 for mode\n3 for Median";
+    public static final String GOODBYE_MESSAGE = "Leaving the program, thanks for using.";
+
+
+    public void announceMessage(String message) {
+        System.out.println(message);
+    }
+
+    public String askUserForNumber() {
+        System.out.println("Please put in your numbers");
+        return scanner.nextLine();
+    }
+
+    public int getOptionInput() {
+        System.out.println("Please pick an option");
+        int userInput = scanner.nextInt();
+        scanner.nextLine();
+        return userInput;
+    }
+
+    public int askUserToContinue() {
+        System.out.println("Please give your decision");
+        int userInput = scanner.nextInt();
+        scanner.nextLine();
+        return userInput;
+
+    }
+}
+
+class MeanMedianModeModel {
+
+    public double[] createArrayOfUserInput(String userInput) {
+        String[] inputArray = userInput.split(",");
+        double[] numbersDouble = new double[inputArray.length];
+        for (int i = 0; i < inputArray.length; i++) {
+            numbersDouble[i] = Integer.parseInt(inputArray[i].trim()); // .trim() removes any extra spaces
+
+        }
+        return numbersDouble;
+    }
+
+    public double meanOfList(double[] arrayOfNumbers) {
+        double sum = 0;
+        for (double arrayOfNumber : arrayOfNumbers) {
+            sum += arrayOfNumber;
+        }
+        return sum / arrayOfNumbers.length;
+    }
+
+    public double modeOfList(double[] arrayOfNumbers) {
+
+        HashMap<Double, Integer> occurrenceOfNumbers = new HashMap<>();
+
+        // Count occurrences of each number in the array
+        for (double number : arrayOfNumbers) {
+            occurrenceOfNumbers.put(number, occurrenceOfNumbers.getOrDefault(number, 0) + 1);
+        }
+
+        // Identify the mode by finding the key with the highest count
+        double mode = arrayOfNumbers[0];
+        int maxCount = 0;
+
+        for (Map.Entry<Double, Integer> entry : occurrenceOfNumbers.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                mode = entry.getKey();  // Set mode to the key (number)
+                maxCount = entry.getValue();  // Update maxCount to the highest count found
+            }
+        }
+
+        return mode;
+    }
+
+    public double medianOfList(double[] arrayOfNumbers) {
+        double median = 0;
+
+        return median;
+    }
+
+
 }
